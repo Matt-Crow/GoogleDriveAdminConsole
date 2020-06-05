cefa01c366660e4569584130f75a158c87784638
IronHeart7334 <greengrappler12@gmail.com>
2020-06-05 12:24:29 -0700

    initial commit

---
+++ src/main/java/start/Main.java
@@ -0,0 +1,15 @@
+package start;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+/**
+ *
+ * @author Matt
+ */
+public class Main {
+    public static void main(String[] args) throws IOException, GeneralSecurityException{
+        System.out.println("running");
+        Test.main(args);
+    }
+}
