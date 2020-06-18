05ff36e3616f8a8a2a55ba94a951ce7d849cbba7
IronHeart7334 <greengrappler12@gmail.com>
2020-06-18 15:29:58 -0700

    Still need to implement cert form info prop file

--- src/main/java/structs/CertificationFormInfo.java
+++ src/main/java/structs/CertificationFormInfo.java
@@ -1,47 +1,70 @@
 package structs;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  *
  * @author Matt
  */
-public final class CertificationFormInfo {
-    private final String fileId;
-    private final String sheetName;
-    private final String nameHeader;
-    private final String emailHeader;
-    private final String mcUserHeader;
-    private final String levelHeader;
+public final class CertificationFormInfo extends Properties{
+    private static final String CERT_FORM_ID_KEY = "spreadsheetId";
+    private static final String RESPONSE_SHEET_NAME_KEY = "responseSheetName";
+    private static final String NAME_HEADER_KEY = "nameHeader";
+    private static final String EMAIL_HEADER_KEY = "emailHeader";
+    private static final String MC_USER_HEADER_KEY = "minecraftUsernameHeader";
+    private static final String LEVEL_HEADER_KEY = "levelHeader";
     
     public CertificationFormInfo(String spreadsheetId, String responseSheetName, String nameCol, String emailCol, String mcUserCol, String levelCol){
-        fileId = spreadsheetId;
-        sheetName = responseSheetName;
-        nameHeader = nameCol;
-        emailHeader = emailCol;
-        mcUserHeader = mcUserCol;
-        levelHeader = levelCol;
+        super();
+        setProperty(CERT_FORM_ID_KEY, spreadsheetId);
+        setProperty(RESPONSE_SHEET_NAME_KEY, responseSheetName);
+        setProperty(NAME_HEADER_KEY, nameCol);
+        setProperty(EMAIL_HEADER_KEY, emailCol);
+        setProperty(MC_USER_HEADER_KEY, mcUserCol);
+        setProperty(LEVEL_HEADER_KEY, levelCol);
+    }
+    
+    public CertificationFormInfo(){
+        this("sheetIdHere", "responseSheetNameHere", "nameHeaderHere", "emailHeaderHere", "mcHeaderHere", "levelHeaderHere");
     }
     
     public String getFileId(){
-        return fileId;
+        return getProperty(CERT_FORM_ID_KEY);
     }
     
     public String getSheetName(){
-        return sheetName;
+        return getProperty(RESPONSE_SHEET_NAME_KEY);
     }
     
     public String getNameHeader(){
-        return nameHeader;
+        return getProperty(NAME_HEADER_KEY);
     }
     
     public String getEmailHeader(){
-        return emailHeader;
+        return getProperty(EMAIL_HEADER_KEY);
     }
     
     public String getMinecraftUsernameHeader(){
-        return mcUserHeader;
+        return getProperty(MC_USER_HEADER_KEY);
     }
     
     public String getParticipationLevelHeader(){
-        return levelHeader;
+        return getProperty(LEVEL_HEADER_KEY);
+    }
+    
+    public void save(File f) throws FileNotFoundException, IOException{
+        store(new FileOutputStream(f), "This file contains information about which Google Sheet contains certification information");
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder bob = new StringBuilder();
+        bob.append("CERTIFICATION FORM INFO:\n");
+        forEach((k, v)->bob.append(String.format("* %s : %s \n", k, v)));
+        return bob.toString();
     }
 }
