4d0baa3c849183daf2a20a80ebff449d40bfc439
IronHeart7334 <greengrappler12@gmail.com>
2020-07-22 13:58:18 -0700

    Next, add more instructions, JAR, and send

--- src/main/java/gui/components/GoogleSheetsPropertyFileCreator.java
+++ src/main/java/gui/components/GoogleSheetsPropertyFileCreator.java
@@ -1,9 +1,35 @@
 package gui.components;
 
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import structs.GoogleSheetProperties;
+import sysUtils.FileSystem;
+import sysUtils.Logger;
+
 /**
  *
  * @author Matt
  */
-public class GoogleSheetsPropertyFileCreator {
+public class GoogleSheetsPropertyFileCreator extends AbstractGoogleSheetsPropertyFileWidget {
+    
+    public GoogleSheetsPropertyFileCreator(String header) {
+        super(header, true, "create");
+    }
+
+    @Override
+    public void buttonPressed() {
+        GoogleSheetProperties gsp = getSelectedProperties();
+        gsp.clear();
+        gsp.setFileId(getFileIdText());
+        gsp.setSheetName(getSheetNameText());
+        
+        try {
+            String path = FileSystem.getInstance().saveProperties(getLocalFileNameText(), gsp);
+            JOptionPane.showMessageDialog(this, String.format("Saved properties to %s", path));
+        } catch (IOException ex) {
+            Logger.logError(ex);
+        }
+        
+    }
 
 }
--- src/main/java/gui/pluginPages/GoogleSheetPropsPage.java
+++ src/main/java/gui/pluginPages/GoogleSheetPropsPage.java
@@ -1,52 +1,22 @@
 package gui.pluginPages;
 
 import gui.MainPane;
-import gui.components.TextInputBox;
+import gui.components.GoogleSheetsPropertyFileCreator;
 import gui.pages.AbstractFormPage;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import structs.GoogleSheetProperties;
-import sysUtils.FileSystem;
-import sysUtils.Logger;
 
 /**
  *
  * @author Matt
  */
 public class GoogleSheetPropsPage extends AbstractFormPage {
-    private final TextInputBox propertyFileName;
-    private final TextInputBox spreadsheetId;
-    private final TextInputBox sheetName;
-    
     public GoogleSheetPropsPage(MainPane inPane) {
         super(inPane);
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        
-        propertyFileName = new TextInputBox("File name", "enter a name for this property file");
-        add(propertyFileName);
-        
-        spreadsheetId = new TextInputBox("Spreadsheet ID", "enter the ID of a Google Spreadsheet here");
-        add(spreadsheetId);
-        
-        sheetName = new TextInputBox("Sheet name", "enter the sheet name");
-        add(sheetName);
-        
-        JButton run = new JButton("Create");
-        run.addActionListener((e)->{
-            submit();
-        });
-        add(run);
+        add(new GoogleSheetsPropertyFileCreator("New Property File"));
     }
 
     @Override
     public void doSubmit() throws Exception {
-        GoogleSheetProperties gsp = new GoogleSheetProperties();
-        gsp.setFileId(spreadsheetId.getInput());
-        gsp.setSheetName(sheetName.getInput());
-        
-        String path = FileSystem.getInstance().saveProperties(propertyFileName.getInput(), gsp);
-        Logger.log(String.format("Saved properties to %s", path));
+        // do nothing: The file creator handles this
     }
 
 }
