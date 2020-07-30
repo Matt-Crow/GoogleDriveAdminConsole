9c375902ef48efb9a0232614e00fa4939a607c32
IronHeart7334 <greengrappler12@gmail.com>
2020-07-30 14:00:46 -0700

    Property creator now has help

--- src/main/java/plugins/implementations/googleSheetPropertyFileCreator/GoogleSheetPropsPlugin.java
+++ src/main/java/plugins/implementations/googleSheetPropertyFileCreator/GoogleSheetPropsPlugin.java
@@ -1,15 +1,19 @@
 package plugins.implementations.googleSheetPropertyFileCreator;
 
+import fileUtils.FileReadWriteUtil;
 import gui.MainPane;
 import gui.pages.AbstractFormPage;
+import java.io.IOException;
 import plugins.utils.AbstractPlugin;
+import sysUtils.Logger;
 
 /**
  *
  * @author Matt
  */
 public class GoogleSheetPropsPlugin implements AbstractPlugin {
-
+    private String cachedHelpText = null;
+    
     @Override
     public AbstractFormPage getFormPage(MainPane pane) {
         return new GoogleSheetPropsPage(pane);
@@ -30,4 +34,17 @@ public class GoogleSheetPropsPlugin implements AbstractPlugin {
         return "creates a new set of Google Sheet properties, which allow the application to locate Google Sheets in your drive";
     }
 
+    @Override
+    public String getHelp(){
+        String ret = "couldn't get help for " + getName();
+        if(cachedHelpText == null){
+            try {
+                cachedHelpText = FileReadWriteUtil.readStream(GoogleSheetPropsPlugin.class.getResourceAsStream("/help/googleSheetPropertyFileCreatorHelp.txt"));
+                ret = cachedHelpText;
+            } catch (IOException ex) {
+                Logger.logError(ex);
+            }
+        }
+        return ret;
+    }
 }
