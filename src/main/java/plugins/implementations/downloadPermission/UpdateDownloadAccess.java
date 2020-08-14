35fa0476371df0dc09aaf2533ca87e8b5fa10c71
IronHeart7334 <greengrappler12@gmail.com>
2020-08-14 14:16:57 -0700

    works crash

--- src/main/java/plugins/implementations/downloadPermission/UpdateDownloadAccess.java
+++ src/main/java/plugins/implementations/downloadPermission/UpdateDownloadAccess.java
@@ -2,6 +2,7 @@ package plugins.implementations.downloadPermission;
 
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.Drive.Permissions;
+import com.google.api.services.drive.DriveRequest;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.Permission;
 import com.google.api.services.drive.model.PermissionList;
@@ -9,11 +10,13 @@ import plugins.implementations.fileListReader.ReadFileList;
 import drive.AbstractDriveCommand;
 import drive.CommandBatch;
 import drive.GoogleDriveFileId;
+import drive.GoogleDriveService;
 import fileUtils.FileList;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import structs.FileInfo;
 import structs.GoogleSheetProperties;
 import sysUtils.Logger;
@@ -59,7 +62,43 @@ public class UpdateDownloadAccess extends AbstractDriveCommand<String[]>{
         return childFiles;
     }
     
-    
+    private Stream<DriveRequest<?>> getChangesToMake(FileInfo info){
+        List<DriveRequest<?>> changesToMake = new ArrayList<>();
+        Drive drive = getServiceAccess().getDrive();
+        Drive.Files files = drive.files();
+        Drive.Permissions perms = drive.permissions();
+        
+        File changes = new File();
+        changes.setViewersCanCopyContent((info.shouldCopyBeEnabled()) ? Boolean.TRUE: Boolean.FALSE);
+        
+        // first, try to create the request to change copy enabled
+        try {
+            Drive.Files.Update updateCopyPerms = files.update(info.getFileId().toString(), changes);
+            changesToMake.add(updateCopyPerms);
+        } catch (IOException ex) {
+            Logger.logError(ex);
+        }
+        
+        // next, try to create the request to remove "anyone with the link can view"
+        try {
+            PermissionList permList = perms.list(info.getFileId().toString()).execute();
+            List<Permission> anyoneWithLink = permList.getPermissions().stream().filter((perm)->perm.getId().equals("anyoneWithLink")).collect(Collectors.toList());
+            List<Permissions.Delete> deleteThose = anyoneWithLink.stream().map((perm)->{
+                Permissions.Delete del = null;
+                try {
+                    del = perms.delete(info.getFileId().toString(), perm.getId());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                return del;
+            }).collect(Collectors.toList());
+            changesToMake.addAll(deleteThose);
+        } catch (IOException ex) {
+            Logger.logError(ex);
+        }
+        
+        return changesToMake.stream().filter((req)->req != null);
+    }
     
     @Override
     public String[] execute() throws IOException {
@@ -73,6 +112,9 @@ public class UpdateDownloadAccess extends AbstractDriveCommand<String[]>{
         Logger.log("Will attempt updating download access for the following files:");
         Logger.log(allLeafNodes.toString());
         
+        List<DriveRequest> allReqs = allLeafNodes.stream().flatMap(this::getChangesToMake).filter((req)->req != null).collect(Collectors.toList());
+        allReqs.forEach(System.out::println);
+        /*
         Drive.Files files = getServiceAccess().getDrive().files();
         
         List<Drive.Files.Update> updates = allLeafNodes
@@ -88,11 +130,14 @@ public class UpdateDownloadAccess extends AbstractDriveCommand<String[]>{
                 }
                 return update;
             }).filter((req)->req != null).collect(Collectors.toList());
-
+        
         // batch requests to add or remove download access for viewers
         CommandBatch<File> batch = new CommandBatch<>(updates);
         List<File> updated = batch.execute();
-        
+        */
+        CommandBatch batch = new CommandBatch(allReqs); // needs diamond operator, but I can't figure out the capture types now
+        List updated = batch.execute();
+        /*
         // remove "anyone with the link can view" options
         Drive.Permissions permissions = getServiceAccess().getDrive().permissions();
         
@@ -121,10 +166,10 @@ public class UpdateDownloadAccess extends AbstractDriveCommand<String[]>{
             }
             return null;
         }).collect(Collectors.toList());
-        
+        */
         Logger.log("Successfully updated download access for the following files:");
-        updated.stream().forEach((f)->Logger.log(String.format("- %s", f.getId())));
-        String[] updatedIds = updated.stream().map((f)->f.getId()).toArray((size)->new String[size]);
+        updated.stream().forEach((f)->Logger.log(String.format("- %s", f)));
+        String[] updatedIds = new String[0];//updated.stream().map((f)->f.getId()).toArray((size)->new String[size]);
         
         return updatedIds;
     }
